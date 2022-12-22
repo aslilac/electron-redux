9b4f0b08fc09dbcf22bdfe216d71e2dc09330060
McKayla Washburn <mckayla@hey.com>
2022-12-21 21:09:00 -0700

    tweak comment

--- tests/e2e/src/main/index.js
+++ tests/e2e/src/main/index.js
@@ -62,7 +62,7 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
-	// On OS X it's common to re-create a window in the app when the
+	// On macOS it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	while (views.length < 2) {
 		createWindow();
